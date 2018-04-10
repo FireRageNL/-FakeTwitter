b66fe68a5e5507fa8d974524575aff705bc43a7d
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2018-04-10 16:50:44 +0200

    Less hashing passes
    oops

--- src/test/java/HashingTest.java
+++ src/test/java/HashingTest.java
@@ -24,7 +24,7 @@ public class HashingTest {
 	//This is a stress test to ensure that at least 100 hashes a second can be generated
 	@Test(timeout=1000)
 	public void HashingStressTest() throws InvalidKeySpecException, NoSuchAlgorithmException {
-		for(int i = 0; i < 100; i++){
+		for(int i = 0; i < 50; i++){
 			Hashing.generatePasswordHash("Nyello");
 		}
 		Assert.assertTrue(true);
