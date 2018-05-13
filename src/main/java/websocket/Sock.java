5747093d587c9bc1502dc97f97e2b3576b6b3392
Roy van Oldenbeek <r.vanoldenbeek@student.fontys.nl>
2018-05-13 14:52:19 +0200

    OnMessage implemented
    IT WORKS :D

--- src/main/java/websocket/Sock.java
+++ src/main/java/websocket/Sock.java
@@ -1,6 +1,11 @@
 package websocket;
 
+import Entities.Account;
+import Logic.Implementations.UserLogic;
+
+import javax.inject.Inject;
 import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
@@ -10,6 +15,9 @@ import java.util.List;
 @ServerEndpoint("/socket/{username}")
 public class Sock {
 
+	@Inject
+	UserLogic ul;
+
 	@OnOpen
 	public void open(Session session, @PathParam("username") String username){
 		Listner.getInstance().getSessionMap().put(username,session);
@@ -18,5 +26,16 @@ public class Sock {
 	@OnClose
 	public void close(Session session, @PathParam("username") String username){
 		Listner.getInstance().getSessionMap().remove(username);
+	}
+
+	@OnMessage
+	public void onMessage(String message, Session session, @PathParam("username") String username){
+		List<String> users = Listner.getInstance().getUsers();
+
+		Account usr = ul.getUserFromDatabase(username);
+
+		users.forEach(user -> {
+				Listner.getInstance().getSessionMap().get(user).getAsyncRemote().sendText(message + "Has been posted by " + username);
+		});
 	}
 }
